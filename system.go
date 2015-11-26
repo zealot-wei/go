d56062c1b4940adf7efdcd45ab98f185eb4d5e89
zealot-wei <zealot-wei@users.noreply.github.com>
2015-11-26 15:59:29 +0800

    Update system.go

--- system.go
+++ system.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-  "fmt"
-  "os/exec"
+	"bytes"
+	"fmt"
+	"os/exec"
+	"syscall"
 )
 
 func exec_without_output(cmd string, args []string) (bool, error) {
@@ -11,6 +13,44 @@ func exec_without_output(cmd string, args []string) (bool, error) {
   }
   return true, nil
 }
+
+func ExecBash(command string, args []string) (int, string, error) {
+	cmd := exec.Command(command, args...)
+	var waitStatus syscall.WaitStatus
+
+	//Stdout buffer
+	cmdOutput := &bytes.Buffer{}
+
+	//Attach buffer to command stdout
+	cmd.Stdout = cmdOutput
+
+	//Execute command
+	err := cmd.Run()
+
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Printf("Command:%s execute error\n", command)
+			//return 2, "", errors.New("command error")
+		}
+	}()
+
+	if err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			waitStatus = exitError.Sys().(syscall.WaitStatus)
+			return waitStatus.ExitStatus(), "", err
+		} else {
+			fmt.Println("something wrong")
+		}
+	}
+	waitStatus = cmd.ProcessState.Sys().(syscall.WaitStatus)
+	return waitStatus.ExitStatus(), cmdOutput.String(), nil
+
+}
+
+
+
+
+
 
 func main() {
   cmd := "cat"
