b3ecb74ed778b0682b6082f6cbdaa6ba7f322d3e
zealot-wei <zealot-wei@users.noreply.github.com>
2015-11-27 17:20:50 +0800

    Update timer.go

--- timer.go
+++ timer.go
@@ -2,56 +2,59 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
-	"os"
 	"os/exec"
 	"strings"
 	"time"
 )
 
-func ExecTimeout(command string, args []string, timeout int) (int, string, error) {
+func ExecTimeout(command string, args []string, timeout int) (string, error, bool) {
 	// Create *Cmd
 	cmd := exec.Command(command, args...)
 	cmdstring := strings.Join(cmd.Args, " ")
 
 	// Stdout buffer
 	buffer := &bytes.Buffer{}
+	result := make(chan error)
 
 	// Attach buffer to command stdout
 	cmd.Stdout = buffer
 
 	// Start command
 	if err := cmd.Start(); err != nil {
-		fmt.Println("Command: %s error when start", cmdstring)
-		return 911, "", err
+		fmt.Printf("Command: %s error when start\n", cmdstring)
+		return "", errors.New("Start command errors"), false
 	}
 
+	go func() {
+		result <- cmd.Wait()
+	}()
+
 	// Create a timer than will kill the process
-	timer := time.NewTimer(time.Second * time.Duration(timeout))
-	go func(timer *time.Timer, cmd *exec.Cmd) {
-		for _ = range timer.C {
-			err := cmd.Process.Signal(os.Kill)
-			if err != nil {
-				fmt.Println("process tiemout\n")
-				fmt.Printf("%V %T %s\n", err, err, err)
-			} else {
-				fmt.Println("ok")
-			}
+	select {
+	case <-time.After(time.Second * time.Duration(timeout)):
+		fmt.Printf("timeout, process: %s will be killed\n", cmdstring)
+
+		go func() {
+			t := <-result
+			fmt.Println("timeout return: ", t)
+		}()
+
+		// timeout, kill the process
+		if err := cmd.Process.Kill(); err != nil {
+			fmt.Printf("failed to kill: %s, error: %s", cmdstring, err)
 		}
-	}(timer, cmd)
-
-	// Jobs after process has finished
-	cmd.Wait()
-	fmt.Printf("%d bytes generated", len(buffer.Bytes()))
-
-	return 0, "", nil
-
+		return "", errors.New("process timeout"), true
+	case <-result:
+		return buffer.String(), nil, false
+	}
 }
 
 func main() {
-	retcode, out, err := ExecTimeout("cat", []string{"/dev/random"}, 4)
-	fmt.Printf("%V %T %d\n", retcode, retcode, retcode)
+	out, err, t := ExecTimeout("ls", []string{"/dev/random"}, 2)
 	fmt.Printf("%V %T %s\n", out, out, out)
 	fmt.Printf("%V %T %s\n", err, err, err)
+	fmt.Println(t)
 
 }
